84180d673b90aa61056bcc0289e5b2fc2cc20ae4
Gwenaël Deniel <monsieur.potatoes93@gmail.com>
2022-11-29 11:51:44 +0100

    Graphique 3 ok graphique 2 en cours

--- src/Controlers/CtrlGraphique.java
+++ src/Controlers/CtrlGraphique.java
@@ -35,6 +35,139 @@ public class CtrlGraphique
         return data;
     }
 
+    public HashMap<String, HashMap<String, Integer>> getDataGraph2() throws SQLException {
+        HashMap<String, HashMap<String, Integer>> data = new HashMap<>();
+        ps = cnx.prepareStatement("SELECT COUNT(employe.numEmp), employe.ageEmp, employe.sexe\n" +
+                "FROM employe\n" +
+                "GROUP BY employe.sexe, employe.ageEmp\n" +
+                "ORDER BY employe.ageEmp");
+        rs = ps.executeQuery();
+        while(rs.next()){
+            if(rs.getString("sexe").compareTo("Homme") == 0){
+                if(data.containsKey("Homme")){
+                    if(rs.getInt("ageEmp") < 20){
+                        if(data.get("Homme").containsKey("< 20")){
+                            data.get("Homme").put("< 20", rs.getInt(1)-data.get("Homme").get("< 20"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("< 20", 0-rs.getInt(1));
+                            data.put("Homme", tranche);
+                        }
+                    } else if(rs.getInt("ageEmp") < 30){
+                        if(data.get("Homme").containsKey("20 - 29")){
+                            data.get("Homme").put("20 - 29", rs.getInt(1)-data.get("Homme").get("20 - 29"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("20 - 29", 0-rs.getInt(1));
+                            data.put("Homme", tranche);
+                        }
+                    } else if(rs.getInt("ageEmp") < 40){
+                        if(data.get("Homme").containsKey("30 - 39")){
+                            data.get("Homme").put("30 - 39", rs.getInt(1)-data.get("Homme").get("30 - 39"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("30 - 39", 0-rs.getInt(1));
+                            data.put("Homme", tranche);
+                        }
+                    } else if(rs.getInt("ageEmp") < 50){
+                        if(data.get("Homme").containsKey("40 - 49")){
+                            data.get("Homme").put("40 - 49", rs.getInt(1)-data.get("Homme").get("40 - 49"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("40 - 49", 0-rs.getInt(1));
+                            data.put("Homme", tranche);
+                        }
+                    } else if(rs.getInt("ageEmp") >= 50){
+                        if(data.get("Homme").containsKey("50+")){
+                            data.get("Homme").put("50+", rs.getInt(1)-data.get("Homme").get("50+"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("50+", 0-rs.getInt(1));
+                            data.put("Homme", tranche);
+                        }
+                    }
+                }
+                else{
+                    HashMap<String, Integer> truc = new HashMap<>();
+                    truc.put("< 20", 0);
+                    data.put("Homme", truc);
+                }
+            }
+            else{
+                if(data.containsKey("Femme")){
+                    if(rs.getInt("ageEmp") < 20){
+                        if(data.get("Femme").containsKey("< 20")){
+                            data.get("Femme").put("< 20", rs.getInt(1)+data.get("Femme").get("< 20"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("< 20", rs.getInt(1));
+                            data.put("Femme", tranche);
+                        }
+                    } else if(rs.getInt("ageEmp") < 30){
+                        if(data.get("Femme").containsKey("20 - 29")){
+                            data.get("Femme").put("20 - 29", rs.getInt(1)+data.get("Femme").get("20 - 29"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("20 - 29", rs.getInt(1));
+                            data.put("Femme", tranche);
+                        }
+                    } else if(rs.getInt("ageEmp") < 40){
+                        if(data.get("Femme").containsKey("30 - 39")){
+                            data.get("Femme").put("30 - 39", rs.getInt(1)+data.get("Femme").get("30 - 39"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("30 - 39", rs.getInt(1));
+                            data.put("Femme", tranche);
+                        }
+                    } else if(rs.getInt("ageEmp") < 50){
+                        if(data.get("Femme").containsKey("40 - 49")){
+                            data.get("Femme").put("40 - 49", rs.getInt(1)+data.get("Femme").get("40 - 49"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("40 - 49", rs.getInt(1));
+                            data.put("Femme", tranche);
+                        }
+                    } else if(rs.getInt("ageEmp") >= 50){
+                        if(data.get("Femme").containsKey("50+")){
+                            data.get("Femme").put("50+", rs.getInt(1)+data.get("Femme").get("50+"));
+                        }
+                        else{
+                            HashMap<String, Integer> tranche = new HashMap<>();
+                            tranche.put("50+", rs.getInt(1));
+                            data.put("Femme", tranche);
+                        }
+                    }
+                }
+                else{
+                    HashMap<String, Integer> truc = new HashMap<>();
+                    truc.put("< 20", 0);
+                    data.put("Femme", truc);
+                }
+            }
+        }
+        return data;
+    }
+
+    public HashMap<String, Double> getDataGraph3() throws SQLException {
+        HashMap<String, Double> data = new HashMap<>();
+        ps = cnx.prepareStatement("SELECT SUM((SELECT COUNT(employe.numEmp) FROM employe WHERE employe.sexe = \"Homme\") / (SELECT COUNT(employe.numEmp) FROM employe) * 100)");
+        rs = ps.executeQuery();
+        if(rs.next()){
+            data.put("Homme", rs.getDouble(1));
+            data.put("Femme", 100-rs.getDouble(1));
+        }
+        return data;
+    }
+
     public HashMap<String, HashMap<String, Double>> getDataGraph4() throws SQLException {
         HashMap<String, HashMap<String, Double>> data = new HashMap<>();
         ps = cnx.prepareStatement("SELECT vente.nomSemestre, SUM(vente.montant), vente.nomMagasin\n" +
--- src/Vues/FrmGraphique.java
+++ src/Vues/FrmGraphique.java
@@ -3,16 +3,12 @@ package Vues;
 import Controlers.CtrlGraphique;
 import Entities.DatasGraph;
 import Tools.ConnexionBDD;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
+import org.jfree.chart.*;
+import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.plot.CombinedDomainXYPlot;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.RingPlot;
+import org.jfree.chart.plot.*;
+import org.jfree.chart.util.Rotation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultKeyedValues2DDataset;
 import org.jfree.data.general.DefaultPieDataset;
@@ -21,6 +17,8 @@ import org.jfree.data.xy.XYZDataset;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.sql.SQLException;
 import java.util.Map;
 
@@ -38,6 +36,7 @@ public class FrmGraphique extends JFrame{
         this.pack();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        this.setSize(1200, 900);
 
         ConnexionBDD cnx = new ConnexionBDD();
         ctrlGraphique = new CtrlGraphique();
@@ -59,9 +58,73 @@ public class FrmGraphique extends JFrame{
                 "Salaire",
                 data,
                 PlotOrientation.VERTICAL, false, true, false);
+        CategoryPlot categoryPlot = chart1.getCategoryPlot();
+        CategoryAxis domainAxis = categoryPlot.getDomainAxis();
+        domainAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
         ChartPanel graph1 = new ChartPanel(chart1);
         pnlGraph1.add(graph1);
         pnlGraph1.validate();
+
+        //Graphique 2
+        data = new DefaultCategoryDataset();
+        String tranche;
+        String sexe;
+        int nb;
+        //On remplie le dataset
+        for(String key: ctrlGraphique.getDataGraph2().keySet()){
+            sexe = key;
+            for(String key2: ctrlGraphique.getDataGraph2().get(key).keySet()){
+                tranche = key2;
+                nb = ctrlGraphique.getDataGraph2().get(key).get(key2);
+                data.setValue(nb, sexe, tranche);
+            }
+        }
+        //On créer le graphique
+        JFreeChart chart2 = ChartFactory.createStackedBarChart(
+                "Pyramide des ages",
+                "Homme/Femmes",
+                "Age",
+                data,
+                PlotOrientation.HORIZONTAL, false, false, false
+        );
+        ChartPanel graph2 = new ChartPanel(chart2);
+        pnlGraph2.add(graph2);
+        pnlGraph2.validate();
+
+
+
+        //Graphique 3
+        DefaultPieDataset data2 = new DefaultPieDataset();
+        double pourcentage;
+        //On remplit le dataset
+        for(String key: ctrlGraphique.getDataGraph3().keySet()){
+            sexe = key;
+            pourcentage = ctrlGraphique.getDataGraph3().get(key);
+            data2.setValue(sexe, pourcentage);
+        }
+        //On créer le graphique
+        JFreeChart chart3 = ChartFactory.createRingChart(
+                "Pourcentage de femmes et d'hommes",
+                data2, false, true, false
+        );
+        //On change la couleur
+        RingPlot ringPlot = (RingPlot) chart3.getPlot();
+        ringPlot.setSectionPaint("Homme", new Color(0, 255, 0));
+        ringPlot.setSectionPaint("Femme", new Color(200, 100, 0));
+        ringPlot.setStartAngle(0);
+        //On affiche le graphique
+        ChartPanel graph3 = new ChartPanel(chart3);
+        pnlGraph3.add(graph3);
+        pnlGraph3.validate();
+        pnlGraph3.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                ringPlot.setStartAngle(ringPlot.getStartAngle() + 1);
+                ChartPanel graph3 = new ChartPanel(chart3);
+                pnlGraph3.add(graph3);
+                pnlGraph3.validate();
+            }
+        });
 
 
         //Graphique 4
